987f86de2579d7f5d33288dc84bf82b2e32692a4
lexypeter <lexypeter000@gmail.com>
2023-02-16 15:38:48 -0800

    L

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with puts function
+ * main - Entry point
  *
- * Return: 0 (success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
      puts("\"Programming is like building a multilingual puzzle");
